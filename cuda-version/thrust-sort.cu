31a84c9ba23038aa6ae664761124278e9304aecf
Shane Panchot <48486097+kctraveler@users.noreply.github.com>
2022-12-12 22:03:54 -0600

    Included a device sort

--- cuda-version/thrust-sort.cu
+++ cuda-version/thrust-sort.cu
@@ -10,40 +10,66 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-/*
-Unlike the other c++ example, this must be run on windows. I had issues with the NVCC compiler on the LUC cluster and decided to use my local workstation. In order to get a timer for profiling I had to use a Windows library.
-*/
+
+// Must be run on Windows
 
 int main(void)
 {
     LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
     LARGE_INTEGER Frequency;
 
-    // Create host vector prior to starting timer
-    thrust::host_vector<double> h_vec(16777216);
-    thrust::generate(h_vec.begin(), h_vec.end(), rand);
+    // Set N For Each Test here
+    int N = 50000000;
+    bool runHost = true;
 
-    // Start timer for profiling
-    QueryPerformanceFrequency(&Frequency);
-    QueryPerformanceCounter(&StartingTime);
+    std::string hostResult;
+    if (runHost) {
+        // Run on the Host Serially
+        thrust::host_vector<int> h_vec(N);
+        thrust::generate(h_vec.begin(), h_vec.end(), rand);
+
+        QueryPerformanceFrequency(&Frequency);
+        QueryPerformanceCounter(&StartingTime);
+
+        thrust::sort(h_vec.begin(), h_vec.end());
+
+        QueryPerformanceCounter(&EndingTime);
+        ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
+        ElapsedMicroseconds.QuadPart *= 1000000;
+        ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
+
+        hostResult = "The elapsed time was " + std::to_string(ElapsedMicroseconds.QuadPart) + " Microseconds.\n";
+    }
+    else {
+        hostResult = "Host version not run.\n";
+    }
+    // GPU VERSION
+
+    thrust::host_vector<int> h_vec2(N);
+
+    LARGE_INTEGER StartingTime2, EndingTime2, ElapsedMicroseconds2;
+    LARGE_INTEGER Frequency2;
+    QueryPerformanceFrequency(&Frequency2);
+    QueryPerformanceCounter(&StartingTime2);
 
     // transfer data to the device
-    thrust::device_vector<double> d_vec = h_vec;
-    
+    thrust::device_vector<int> d_vec = h_vec2;
+    thrust::generate(h_vec2.begin(), h_vec2.end(), rand);
+
     // use thrust to sort the device array.
     thrust::sort(d_vec.begin(), d_vec.end());
-    // sort on the host
-    //std::sort(h_vec.begin(), h_vec.end());
-
+  
     // transfer data back to host
-    thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+    thrust::copy(d_vec.begin(), d_vec.end(), h_vec2.begin());
 
-    // End timer and calculat Microseconds
-    QueryPerformanceCounter(&EndingTime);
-    ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
-    ElapsedMicroseconds.QuadPart *= 1000000;
-    ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
-    std::cout << "The elapsed time was " <<  ElapsedMicroseconds.QuadPart;
+    QueryPerformanceCounter(&EndingTime2);
+    ElapsedMicroseconds2.QuadPart = EndingTime2.QuadPart - StartingTime2.QuadPart;
+    ElapsedMicroseconds2.QuadPart *= 1000000;
+    ElapsedMicroseconds2.QuadPart /= Frequency2.QuadPart;
+
+    std::string deviceResult = "The elapsed time for GPU Sort was " + std::to_string(ElapsedMicroseconds2.QuadPart) + " Microseconds.\n";
+
+    std::cout << hostResult << deviceResult;
 
     return 0;
 }
